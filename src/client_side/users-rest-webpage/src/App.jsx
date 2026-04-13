515bac6968ccd18a32eccb0ec4c9592b51005c55
Bilal Vandenberge <jakelevrai@outlook.be>
2026-04-13 15:32:44 -0400

    Signup form

--- src/client_side/users-rest-webpage/src/App.jsx
+++ src/client_side/users-rest-webpage/src/App.jsx
@@ -35,6 +35,250 @@ function CreationForm({ token, modEnable }) {
   const [formKey, setFormKey] = useState(0);
   const [pwTooltip, setPwTooltip] = useState(false);
   const [pwLength, setPWLength] = useState(15);
+  const [rgp, setRgp] = useState(false);
+  const [errortype, setErrorType] = useState(false);
+
+  useEffect(() => {
+    if (generating) {
+      setCreatingErrorVisible(false);
+      setTimeout(() => {
+        console.log("pw: " + pwLength);
+        fetch("http://localhost:3225/motdepasse/" + pwLength, {
+          method: "GET",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          }
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            if (response.return === 322500) {
+              setUser(prev => ({ ...prev, "password": response.password }));
+              setRgp(true);
+              setPwTooltip(false);
+            } else {
+              changeErrorCode(response.return);
+            }
+          })
+          .catch((err) => {
+            console.log("Error: " + err);
+            if (err.message.includes("NetworkError")) {
+              changeError("Network error");
+            } else {
+              changeError("Error");
+            }
+          })
+          .then(() => {
+            setGenerating(false);
+          })
+      }, 500);
+    }
+
+    if (creating) {
+      // Send login request
+      setCreatingErrorVisible(false);
+      // setCreatingError("");
+      setTimeout(() =>
+        fetch("http://localhost:3225/profils", {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': token
+          },
+          body: JSON.stringify(user)
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            if (response.return === 322500) {
+              const registered_email = user.email || "user";
+              setUser({
+                "email": "",
+                "username": "",
+                "password": "",
+                "isadmin": false,
+              });
+              setFormKey(formKey + 1); // Clear the form
+              setErrorType(true);
+              changeError(registered_email + " successfully created");
+            } else {
+              setErrorType(false);
+              changeErrorCode(response.return);
+            }
+          })
+          .catch((err) => {
+            console.log("Error: " + err);
+            if (err.message.includes("NetworkError")) {
+              setErrorType(false);
+              changeError("Network error");
+            } else {
+              setErrorType(false);
+              changeError("Error");
+            }
+          })
+          .then(() => {
+            setCreating(false);
+          }), 500);
+    }
+  }, [creating, generating]);
+
+  function changeError(new_error) {
+    setCreatingErrorVisible(false);
+
+    setTimeout(() => {
+      setCreatingError(new_error)
+      setCreatingErrorVisible(true);
+    }, 150);
+  };
+
+  function changeErrorCode(new_error_code) {
+    const errorTxt = getErrorMessage(new_error_code)
+
+    setCreatingErrorVisible(false);
+
+    setTimeout(() => {
+      setCreatingError(errorTxt)
+      setCreatingErrorVisible(true);
+    }, 150);
+  };
+
+  function submitButtonClicked(e) {
+    if (!creating) {
+      setCreating(true);
+    }
+    e.preventDefault();
+  }
+
+  function showRandomTooltip(e) {
+    setPwTooltip(!pwTooltip)
+    e.preventDefault();
+  }
+
+  function gobackButtonClicked(e) {
+    modEnable(false);
+  }
+
+  function generateButtonClicked(e) {
+    console.log("generating");
+    setGenerating(true);
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <div className="login_page">
+        <div className="big_main_title">
+          <svg className="big_main_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>account-box</title><path width={24} height={24} fill="#dadada" d="M6,17C6,15 10,13.9 12,13.9C14,13.9 18,15 18,17V18H6M15,9A3,3 0 0,1 12,12A3,3 0 0,1 9,9A3,3 0 0,1 12,6A3,3 0 0,1 15,9M3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3H5C3.89,3 3,3.9 3,5Z" /></svg>
+          <div className="big_main_dual">
+            <h1>
+              users-rest-api
+            </h1>
+            <h3 className="big_main_subtitle">
+              IFT3225 Projet 2
+            </h3>
+          </div>
+        </div>
+
+        <form className="creation_box" method='post' key={formKey}>
+          <div className="login_titleparagraph">
+            <h2 className="creation_title">User creation form</h2>
+            <span className="login_paragraph">This form insert a new user into the database.</span>
+          </div>
+          <div className="login_labelandinput">
+            <label className="login_label">username</label>
+            <input className="creation_input" placeholder="craig" type="text" id="username" onChange={e => {
+              setUser(prev => {
+                return { ...prev, username: e.target.value }
+              })
+            }}></input>
+          </div>
+          <div className="login_labelandinput">
+            <label className="login_label">email</label>
+            <input className="creation_input" placeholder="craig@example.com" type="text" id="email" onChange={e => {
+              setUser(prev => {
+                return { ...prev, email: e.target.value }
+              })
+            }}></input>
+          </div>
+          <div className="login_labelandinput">
+            <label className="login_label">password</label>
+            <div className="creation_pw_input">
+              <input className="inner_pw_input" value={user.password} placeholder="a strong one" type={rgp ? "text" : "password"} id="password" onChange={e => {
+                setRgp(false);
+                setUser(prev => {
+                  return { ...prev, password: e.target.value }
+                })
+              }}></input>
+              <div className="wrapper">
+                {pwTooltip && (
+                  <div className="tooltip">
+                    <label className="pw_title">random password</label>
+                    <div className="pw_range_box">
+                      <input
+                        type="range"
+                        min="1" max="60"
+                        value={pwLength}
+                        onChange={(e) => setPWLength(e.target.value)}
+                      />
+                      <label className="pw_value">{pwLength}</label>
+                    </div>
+                    <button className="gen_button" onClick={e => {
+                      generateButtonClicked(e)
+                    }} disabled={creating || generating}>{generating ? <span className="spinner" /> : "Generate"}</button>
+
+                    {/* caret */}
+                    <div className="tooltip-arrow-outline" />
+                    <div className="tooltip-arrow" />
+                  </div>
+                )}
+
+                {/* Target div */}
+                <button className="dice_btn" onClick={e => showRandomTooltip(e)} >
+                  <svg className="dice_svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M141.4 2.3C103-8 63.5 14.8 53.3 53.2L2.5 242.7C-7.8 281.1 15 320.6 53.4 330.9l189.5 50.8c38.4 10.3 77.9-12.5 88.2-50.9l50.8-189.5c10.3-38.4-12.5-77.9-50.9-88.2L141.4 2.3zm23 205.7a32 32 0 1 1 55.4-32 32 32 0 1 1 -55.4 32zM79.2 220.3a32 32 0 1 1 32 55.4 32 32 0 1 1 -32-55.4zm185 96.4a32 32 0 1 1 -32-55.4 32 32 0 1 1 32 55.4zm9-208.4a32 32 0 1 1 32 55.4 32 32 0 1 1 -32-55.4zm-121 14.4a32 32 0 1 1 -32-55.4 32 32 0 1 1 32 55.4zM418 192L377.4 343.2c-17.2 64-83 102-147 84.9l-38.3-10.3 0 30.2c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64L418 192z" /></svg>
+                </button>
+              </div>
+              {/* <button className="dice_btn" onClick={e => showRandomTooltip(e)} >
+                <svg className="dice_svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M141.4 2.3C103-8 63.5 14.8 53.3 53.2L2.5 242.7C-7.8 281.1 15 320.6 53.4 330.9l189.5 50.8c38.4 10.3 77.9-12.5 88.2-50.9l50.8-189.5c10.3-38.4-12.5-77.9-50.9-88.2L141.4 2.3zm23 205.7a32 32 0 1 1 55.4-32 32 32 0 1 1 -55.4 32zM79.2 220.3a32 32 0 1 1 32 55.4 32 32 0 1 1 -32-55.4zm185 96.4a32 32 0 1 1 -32-55.4 32 32 0 1 1 32 55.4zm9-208.4a32 32 0 1 1 32 55.4 32 32 0 1 1 -32-55.4zm-121 14.4a32 32 0 1 1 -32-55.4 32 32 0 1 1 32 55.4zM418 192L377.4 343.2c-17.2 64-83 102-147 84.9l-38.3-10.3 0 30.2c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64L418 192z" /></svg>
+              </button> */}
+            </div>
+          </div>
+          <div className="admincheck_box">
+            <label className="login_label">administrator</label>
+
+            <label className="admin_switch">
+              <input className="admin_checkbox" type="checkbox" id="isadmin" onChange={e => {
+                setUser(prev => {
+                  return { ...prev, isadmin: e.target.value === "on" }
+                })
+              }}></input>
+              <span className="slider round"></span>
+            </label>
+
+
+          </div>
+          <button className="login_button" onClick={e => submitButtonClicked(e)} disabled={creating || generating} >{creating ? <span className="spinner" /> : "Create"}</button>
+          {/* <button className="cancel_button" onClick={e => gobackButtonClicked(e)} disabled={creating || generating} >Go back to documentation</button> */}
+          <p className={`login_note_p ${errortype ? "login_success_p" : "login_failed_p"} ${creatingErrorVisible ? "" : "fade_out"}`}>{creatingError}</p>
+        </form>
+      </div>
+    </>
+  )
+}
+
+function SignupForm({ token, modEnable }) {
+  const [user, setUser] = useState({
+    "email": "",
+    "username": "",
+    "password": "",
+    "isadmin": false,
+  });
+  const [creating, setCreating] = useState(false);
+  const [generating, setGenerating] = useState(false);
+  const [creatingError, setCreatingError] = useState("");
+  const [creatingErrorVisible, setCreatingErrorVisible] = useState("");
+  const [formKey, setFormKey] = useState(0);
+  const [pwTooltip, setPwTooltip] = useState(false);
+  const [pwLength, setPWLength] = useState(15);
   const [rgp, setRgp] = useState(false)
 
   useEffect(() => {
@@ -174,8 +418,8 @@ function CreationForm({ token, modEnable }) {
 
         <form className="creation_box" method='post' key={formKey}>
           <div className="login_titleparagraph">
-            <h2 className="login_title">User creation form</h2>
-            {/* <span className="login_paragraph">Create a user with this form</span> */}
+            <h2 className="login_title">Create your account</h2>
+            <span className="login_paragraph">This form signs you up by inserting a new user into the database.</span>
           </div>
           <div className="login_labelandinput">
             <label className="login_label">username</label>
@@ -249,9 +493,9 @@ function CreationForm({ token, modEnable }) {
 
 
           </div>
-          <button className="login_button" onClick={e => submitButtonClicked(e)} disabled={creating || generating} >{creating ? <span className="spinner" /> : "Create"}</button>
+          <button className="green_login_button" onClick={e => submitButtonClicked(e)} disabled={creating || generating} >{creating ? <span className="green_spinner" /> : "Sign up"}</button>
           <button className="cancel_button" onClick={e => gobackButtonClicked(e)} disabled={creating || generating} >Cancel</button>
-          <p className={`login_failed_p ${creatingErrorVisible ? "" : "fade_out"}`}>{creatingError}</p>
+          <p className={`login_note_p login_failed_p ${creatingErrorVisible ? "" : "fade_out"}`}>{creatingError}</p>
         </form>
       </div>
     </>
@@ -560,7 +804,7 @@ function AuthForm({ modToken, modUser, modCreating }) {
             }}></input>
           </div>
           <button className="login_button" onClick={e => submitButtonClicked(e)} disabled={login} >{login ? <span className="spinner" /> : "Sign in"}</button>
-          <p className={`login_failed_p ${authErrorVisible ? "" : "fade_out"}`}>{authError}</p>
+          <p className={`${authErrorVisible ? "" : "fade_out"} login_failed_p login_note_p`}>{authError}</p>
         </form>
       </div>
     </>
@@ -701,6 +945,7 @@ function App() {
   });
   const [getme, setGetme] = useState(false);
   const [creating, setCreating] = useState(false);
+  const [signuping, setSignuping] = useState(false);
   const [deleting, setDeleting] = useState(false);
 
   useEffect(() => {
@@ -771,14 +1016,20 @@ function App() {
     setDeleting(true);
   }
 
-  if (creating) {
+  if (signuping) {
     return (
       <>
-        <CreationForm token={token} modEnable={setCreating} />
+        <SignupForm token={token} modEnable={setSignuping} />
       </>
     )
   } else if (token) {
-    if (deleting) {
+    if (creating) {
+      return (
+        <>
+          <CreationForm token={token} modEnable={setCreating} />
+        </>
+      )
+    } else if (deleting) {
       return (
         <>
           <DeletionForm modEnable={setDeleting} token={token} isAdmin={user.isadmin} />
@@ -799,7 +1050,7 @@ function App() {
   } else {
     return (
       <>
-        <AuthForm modToken={setToken} modUser={setUser} modCreating={setCreating} />
+        <AuthForm modToken={setToken} modUser={setUser} modCreating={setSignuping} />
       </>
     )
   }
